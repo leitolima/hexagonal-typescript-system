7eb3b8559b560611c89a0a17559e77a092a26240
Leonel <leonel_lima19@hotmail.com>
2023-02-07 12:14:32 -0300

    First commit - hexagonal structure

---
+++ src/users/infrastructure/user-repository/in-memory-user-repository.ts
@@ -0,0 +1,15 @@
+import { User } from "../../domain/user";
+import { UserRepository } from "../../domain/user-repository";
+import { USERS } from "./users";
+
+export class InMemoryUserRepository implements UserRepository {
+  async getByEmail(email: string): Promise<User | null> {
+    const user: User|undefined = USERS.find((user) => user.email === email);
+
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  }
+}
---
+++ src/users/infrastructure/user-repository/users.ts
@@ -0,0 +1,7 @@
+import { User } from "../../domain/user";
+
+export const USERS: User[] = [
+  new User('1', 'user1@hotmail.com', 'Password123$'),
+  new User('2', 'user2@hotmail.com', 'Password123$'),
+  new User('3', 'user3@hotmail.com', 'Password123$'),
+]
